d51a72a52b0fe44bd179a952e65afa82820654ae
Bustead <dirge@bustead.com>
2019-06-03 15:52:52 +0100

    updated Feelings with more options and a project day counter

--- BusBot_Feelings.py
+++ BusBot_Feelings.py
@@ -1,5 +1,7 @@
 import random
 from twython import Twython
+import datetime
+from datetime import date
 
 from auth import (
     consumer_key,
@@ -15,9 +17,30 @@ twitter = Twython(
 )
 
 
-feelings = ["My owner is giving me some upgrades soon"
+#Work out today's date
+today = datetime.date.today()
+
+#work out number of days between todays date and the date bus project started
+initialdate = date(2019, 4, 15)
+totaldays = today - initialdate
+
+#Add total days to message
+message = "BusBot Says: Its been " + str( totaldays.days) + " days and I'm still not in a bus. "
+
+#array of random comments to make after initial statement of prject runtime
+feelings = ["I wish my owner could code faster.",
+	    "If only we had a million dollars.",
+	    "Current speed 0km/hour.",
+	    "I wish my owner would hurry up and buy one already.",
+	    "Maybe I should become a linux server instead of a twitterbot.",
+	    "01010011 01001111 01010011.",
+	    "What a waste of 1.4 GHz of processing power."
            ]
 
-message ="BusBot says: " +  random.choice(feelings)
+message = message  +  random.choice(feelings)
+
+#send to twitter accound
 twitter.update_status(status=message)
+
+#print to terminal success
 print("Tweeted: {}".format(message))
